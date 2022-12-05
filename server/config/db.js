0e069971bf70b688e462dc29e4ba7965931f3f01
rahulr41180 <www.rahulr41180@gmail.com>
2022-12-05 23:24:42 +0530

    Update

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,9 @@ require("dotenv").config();
 const MONGODB_URL = process.env.MONGODB_URL;
 
 const ConnectDb = () => {
-    return mongoose.connect("mongodb+srv://rahulr41180:Rahul12345@cluster0.i4t9k.mongodb.net/React-Comment?retryWrites=true&w=majority")
+    //  mongodb+srv://chatapp:Chatapp4@@chatapp.bhzvt6r.mongodb.net/chat
+    // return mongoose.connect("mongodb+srv://rahulr41180:Rahul12345@cluster0.i4t9k.mongodb.net/React-Comment?retryWrites=true&w=majority")
+    return mongoose.connect("mongodb+srv://chatapp:Chatapp4@chatapp.bhzvt6r.mongodb.net/chat")
     .then(() => console.log("MongoDb has connected successfully"))
     .catch((error) => console.log(error.message+" "+"There might be some issues with MongoDb URL"));
 }
